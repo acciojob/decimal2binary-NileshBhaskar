9762bea51f1af22c8193d8015d4a768e89138bc3
Nilesh Bhaskar <73060066+NileshBhaskar@users.noreply.github.com>
2023-10-13 12:45:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function decimalToBinary(num) {
   //Write you code here
+	let str = "";
+	while(num>0){
+	   // console.log(num%2);
+		str = str + num%2;
+		num = Math.floor(num/2);
+	}
+	let arr = str.split("");
+	arr.reverse();
+    return arr.join("");
   
 }
 
